fe40fe0e7501e024f979c7a563198acf05ff5b2d
fady <gamilfady605@gmail.com>
2022-08-29 20:20:18 +0200

    Adding The Identity Table

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,8 +1,9 @@
 using jwtToken.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 namespace jwtToken.Data
 {
-   public class AppDbContext : DbContext
+   public class AppDbContext : IdentityDbContext
    {
       public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
       {
--- Migrations/20220827235259_adding the identity table.Designer.cs
+++ Migrations/20220827235259_adding the identity table.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using jwtToken.Data;
@@ -11,9 +12,10 @@ using jwtToken.Data;
 namespace auth_jwt_api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220827235259_adding the identity table")]
+    partial class addingtheidentitytable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
